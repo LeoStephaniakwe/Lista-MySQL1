924b2831d640d507d087d086fa60d14905738ca4
Leonardo Moreira <leonardomoreira050@gmail.com>
2018-07-05 08:52:38 -0300

    MySQL Exercicios concluidos!

---
+++ ExercicioMySQL/Exercicio01.sql
@@ -0,0 +1,44 @@
+drop database if exists Exercicio01;
+create database Exercicio01;
+use Exercicio01;
+create table pessoas(
+nome varchar(100),
+cpf varchar(14),
+rg varchar(12),
+data_de_nascimento Date,
+idade tinyint
+);
+
+
+insert into pessoas value 
+('Erick Bryan Enrico Vieira','699.734.958-70','90.745.276-0','28/05/1989',29);
+insert into pessoas value 
+('Samuel Ruan Dias','802.790.194-40','55.318.691-7','08/02/1988',30);
+insert into pessoas value 
+('Fábio Benjamin Souza','522.445.349-60','54.715.232-2','22/03/1970',48);
+insert into pessoas value 
+('Elias Miguel Aparicio','293.928.821-65','12.680.444-8','07/06/1986',32);
+insert into pessoas value 
+('Alexandre Gustavo Cardoso','617.408.878-24','8.888.574-4','15/04/2003',15);
+insert into pessoas value 
+('Kauê Lucca Duarte','943.163.990-47','46.977.509-9','15/06/1965',53);
+insert into pessoas value 
+('Marcos Bryan Guilherme Mendes','955.129.235-95','84.050.077-4','06/02/2015',3);
+insert into pessoas value 
+('Benedito Pedro Carlos da Mota','510.505.472-50','43.588.124-3','19/04/1981',37);
+insert into pessoas value 
+('Luís Igor Barbosa','370.570.311-06','40.651.407-0','06/05/2013',5);
+insert into pessoas value 
+('José Heitor Ramos','675.624.925-81','83.962.258-2','11/04/1965',53);
+insert into pessoas value
+('Lucca Tiago Galvão','788.124.508-57','3.293.335-6','17/06/1977',41);
+insert into pessoas value
+('Tiago Edson Oliveira','389.969.352-39','8.198.446-9','09/06/1977',41);
+insert into pessoas value
+('Enrico Julio Fábio Martins','529.078.166-83','99.886.242-3','11/03/2017',1);
+insert into pessoas value
+('Oliver Victor Benjamin da Mota','798.556.805-02','5.103.578-9','27/03/1985',33);
+insert into pessoas value
+('Bryan Caio Lopes','213.217.201-30','12.949.454-9','18/06/1970',48);
+
+
---
+++ ExercicioMySQL/Exercicio03.sql
@@ -0,0 +1,27 @@
+drop database if exists Exercicio03;
+create database Exercicio03;
+use Exercicio03;
+
+create table champions(
+nome varchar (100),
+descrição  varchar (100),
+habilidade1 varchar (100),
+habilidade2 varchar (100),
+habilidade3 varchar (100),
+habilidade4 varchar (100));
+
+
+insert into champions value ('Katarina','a Lâmina Sinistra','Voracidade','Preparação','Shunpo','Lótus da Morte');
+insert into champions value ('Yasuo','','Estilo do Errante','Tempestade de Aço','Parede de Vento','Espada Ágil','Último Suspiro');
+insert into champions value ('Master Yi','o Espadachim Wuju','Ataque Duplo','','','');
+insert into champions value ('Vayne','a Caçadora Noturna','Rolamento','Dardos de Prata','Condenar','Hora Final');
+insert into champions value ('Lee Sin', 'o Monge Cego', 'Agitação', 'Onda Sônica/Ataque Ressonante', 'Proteger/Vontade de Ferro', 'Tempestade/Mutilar','');
+insert into champions value ('Vi','a Defensora de Piltover','Blindagem','','Pancada Certeira','Força Excessiva','Saque e Enterrada');
+insert into champions value ('Diana','o Escárnio da Lua','Espada de Prata Lunar','Golpe Crescente','Cascata Lívida','Colapso Minguante','Zênite Lunar');
+insert into champions value ('Annie','a Criança Sombria','Piromania','Desintegrar','Incinerar','Escudo Fundido','Invocar Tibbers');
+insert into champions value ('Aatrox','','Poço de Sangue','Voo Sombrio','Sede de Sangue / Preço em Sangue','Lâminas da Aflição','Massacre');
+
+
+
+
+
